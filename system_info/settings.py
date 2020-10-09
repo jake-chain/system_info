f2fa44cda02b0581fa27fee48fe4dc2d68a6800b
Gustavo <dev.gustavo.oliveira@gmail.com>
2020-10-09 17:50:10 -0300

    #001 - System Info - Criando projeto; - Fazendo leitura do arquivo informado pelo usuário; - Configurando a visualização;

--- system_info/settings.py
+++ system_info/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'check this line here'
+SECRET_KEY = 'w*p681tk0_&!tui=iglaqp)t@7=!(h&js6&=h&+$)h^!vkaj1o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
